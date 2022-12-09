9e720c5e8c14049983a3d2fe0c40812762aab63a
Hachu-spatb <polinaander@yandex.ru>
2022-12-09 16:41:31 +0300

    added visual

--- src/visual.py
+++ src/visual.py
@@ -1,48 +1,26 @@
-
 import tkinter as tk
+root = tk.Tk()
+root.title("Форма расчета")
+root.geometry("400x200")
 
-# Создается новое окно с заголовком.
-window = tk.Tk()
-window.title("Форма расчета")
+def getTextInput():
+    result=textExample.get("1.0","end")
+    return 
 
-# Создается новая рамка `frm_form` для ярлыков с текстом и
-# Однострочных полей для ввода информации об адресе.
-frm_form = tk.Frame(relief=tk.SUNKEN, borderwidth=3)
+entry = tk.Entry(width=100, bg="white", fg="black")
+entry.pack()
 
-# Помещает рамку на окно приложения.
-frm_form.pack()
+entry.insert(0, "Введите: "
+                "массу первой звезды, "
+                "массу второй звезды, "
+                "радиус")
 
-# Список ярлыков полей.
-labels = [
-    "Масса первой звезды:",
-    "Масса второй звезды:",
-    "Радиус:",
+textExample=tk.Text(root, height=10)
+textExample.pack()
+btnRead=tk.Button(root, height=2, width=10, text="Ввод",
+                    command=getTextInput)
 
-]
+btnRead.pack()
 
-# Цикл для списка ярлыков полей.
-for idx, text in enumerate(labels):
-    # Создает ярлык с текстом из списка ярлыков.
-    label = tk.Label(master=frm_form, text=text)
-    # Создает текстовое поле которая соответствует ярлыку.
-    entry = tk.Entry(master=frm_form, width=50)
-    label.grid(row=idx, column=0, sticky="e")
-    entry.grid(row=idx, column=1)
-
-# Создает новую рамку `frm_buttons` для размещения в ней
-# кнопок "Отправить" и "Очистить".
-frm_buttons = tk.Frame()
-frm_buttons.pack(fill=tk.X, ipadx=5, ipady=5)
-
-# Создает кнопку "Отправить" и размещает ее
-# справа от рамки `frm_buttons`.
-btn_submit = tk.Button(master=frm_buttons, text="ввод")
-btn_submit.pack(side=tk.RIGHT, padx=10, ipadx=10)
-
-# Создает кнопку "Очистить" и размещает ее
-# справа от рамки `frm_buttons`.
-btn_clear = tk.Button(master=frm_buttons, text="отмена")
-btn_clear.pack(side=tk.RIGHT, ipadx=10)
-
-# Запуск приложения.
-window.mainloop()
+root.mainloop()
+result=textExample.get("1.0", "end")
