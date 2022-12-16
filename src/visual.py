1e9153e73bc2d1b6abfae2681339df5e584121ab
Hachu-spatb <polinaander@yandex.ru>
2022-12-16 15:32:26 +0300

    input without widgets

--- src/visual.py
+++ src/visual.py
@@ -1,26 +1,52 @@
-import tkinter as tk
-root = tk.Tk()
-root.title("Форма расчета")
-root.geometry("400x200")
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from sympy import solve, Symbol
 
-def getTextInput():
-    result=textExample.get("1.0","end")
-    return 
+def volume_counter(lobe, l1, limit, step):
+    counter = 0
+    for x in range(int(limit / step / 3), int((l1 + limit) / step)):
+        for y in lobe[x]:
+            for el in y:
+                if el > 0:
+                    counter += 1
+    return counter / (limit / step) ** 3
 
-entry = tk.Entry(width=100, bg="white", fg="black")
-entry.pack()
+print('Масса первой звезды в массах Солнца')
+m1 = float(input())
+print('Масса второй звезды в массах Солнца')
+m2 = float(input())
+print('Большая полуось системы в а.е.')
+a = float(input())
 
-entry.insert(0, "Введите: "
-                "массу первой звезды, "
-                "массу второй звезды, "
-                "радиус")
+m1s = np.arange(1, 100, 20)
+m2s = np.arange(1, 100, 20)
+a_s = np.arange(1, 6, 1)
+ans = [[],[],[],[]]
+_ = 1
+for m1_ in m1s:
+    for m2_ in m2s:
+        for a_ in a_s:
+            #print(_)
+            limit = a_
+            step = 0.05
+            lobe, xg, yg, zg, l1_ = lobe_(m1_, m2_, float(a_), limit, step)
+            #volume = volume_counter(lobe, l1_, limit, step)
+            ans[0].append(m1_)
+            ans[1].append(m2_)
+            ans[2].append(a_)
+            #ans[3].append(volume)
+            _ += 1
 
-textExample=tk.Text(root, height=10)
-textExample.pack()
-btnRead=tk.Button(root, height=2, width=10, text="Ввод",
-                    command=getTextInput)
 
-btnRead.pack()
+n = m1 / m2
 
-root.mainloop()
-result=textExample.get("1.0", "end")
+limit = a
+step = 0.1
+lobe, xg, yg, zg, l1_ = lobe_(m1, m2, float(a), limit, step)
+
+#volume = volume_counter(lobe, l1_, limit, step)
+#print(volume)
+#print(ans)
+
+model(m1, m2, a, limit, step)
